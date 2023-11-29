344dbb2a67ef70dc32b204550876c71c9290ca6e
Juliang325 <133649655+Juliang325@users.noreply.github.com>
2023-11-29 20:44:26 +0100

    primera migracion

--- app/src/main/java/com/example/pruebecitaroom/database/AppDB.kt
+++ app/src/main/java/com/example/pruebecitaroom/database/AppDB.kt
@@ -4,12 +4,14 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.pruebecitaroom.modelo.daos.DaoEquipo
 import com.example.pruebecitaroom.modelo.daos.DaoJugador
 import com.example.pruebecitaroom.modelo.entities.Equipo
 import com.example.pruebecitaroom.modelo.entities.Jugador
 
-@Database(entities = [Jugador::class, Equipo::class], version = 1)
+@Database(entities = [Jugador::class, Equipo::class], version = 2)
 abstract class AppDB : RoomDatabase() {
     abstract fun DaoJugador(): DaoJugador
     abstract fun DaoEquipo(): DaoEquipo
@@ -26,21 +28,21 @@ abstract class AppDB : RoomDatabase() {
                     .allowMainThreadQueries()
                     //Con migracion hay que incrementar la version y
                     //añadirle que migracion (de la 1 a la 2)
-                    //.addMigrations(MIGRATION_1_2)
+                    .addMigrations(MIGRATION_1_2)
                     //futuras migraciones irían aquí
                     //.addMigrations(MIGRATION_2_3)
                     .build()
             }
 
             return INSTANCE as AppDB
-        }/*
+        }
 
         val MIGRATION_1_2: Migration = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE usuario ADD COLUMN direccion TEXT")
+                database.execSQL("ALTER TABLE jugadores_table ADD COLUMN posicion TEXT")
             }//fin de migrate
         }//fin de Migration
 
-        */
+
     }//fin de companion object
 }//fin de AppDB
